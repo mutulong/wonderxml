22c31d925de1b8592198b822e3f07645f1675a14
mutulong <mutulong@eb8e1895-12bb-0c27-9d5b-08e0f8a90d4c>
2010-06-16 04:14:44 +0000

    Add a new test case

--- wonderxml/Classes/WonderXMLAppDelegate.m
+++ wonderxml/Classes/WonderXMLAppDelegate.m
@@ -28,11 +28,11 @@
 	NSString *testUserString = @"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><User xmlns:=\"http://bluepinme.com\"><billingSchedule>00 00 08 * * *</billingSchedule><brandID>9908315</brandID><changePassword>false</changePassword><expiration>1970-01-01T00:00:00Z</expiration><lastLogIn>1970-01-01T00:00:00Z</lastLogIn><password>bbbb</password><profileID>1968515</profileID><userID>1831917</userID><username>fffffff</username></User>";
 	
 	NSMutableArray *userArray = [parser fromXml:testUserString withObject:user];
-	NSLog(@"userArray :: %@", userArray);
+	NSLog(@"userArray :: %@", [userArray objectAtIndex:0]);
 	
 	//object to xml string
 	
-	
+	user = [userArray objectAtIndex:0];
 	NSNumberFormatter * numberFomatter = [[[NSNumberFormatter alloc] init] autorelease];
 	[numberFomatter setNumberStyle:NSNumberFormatterDecimalStyle];
 	NSNumber * aNumber = [numberFomatter numberFromString:@"123456"];
